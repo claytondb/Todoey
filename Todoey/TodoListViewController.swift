df88c6ae3a9030dc7f8f4478acec3f084aaf62b2
Clayton, David <Clayton_David@shure.com>
2018-02-20 21:49:41 -0600

    Added persistent data with user defaults method.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,8 +12,15 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = ["Buy some milk","Wash clothes","Finish everything"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
+        
 
     }
     
@@ -66,7 +73,9 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
             // stuff that happens when user taps add
-            self.itemArray.append(textField.text! ?? "New item")
+            self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
         }

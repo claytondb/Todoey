181cc9e2ac7ec99f3c16f806a51ee86ea5a1824a
Clayton, David <Clayton_David@shure.com>
2018-02-22 21:39:43 -0600

    Added persistent data via CoreData - got CRUD figured out (Create, Read, Update, Destroy). Writes to SQLite database and reads/updates fine.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,20 +7,17 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
-// The way you're saving data is by encoding the array of data into a plist (property list) and then decoding it back into the array when the app is loaded.
     
     var itemArray = [Item]()
-    
-//    let defaults = UserDefaults.standard
-    
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath)
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
         loadItems()
 
@@ -53,24 +50,15 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        // Print the row number of index path inside item array.
-//        print(itemArray[indexPath.row])
-        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            // Remove checkmark if there is one
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-            // Add checkmark accessory to the row when it's selected
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
+        saveItems()
         
         // Don't keep the row selected
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        self.saveItems()
         
     }
     
@@ -85,8 +73,10 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
             // stuff that happens when user taps add
             
-            let newItem = Item()
+            
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
             self.itemArray.append(newItem)
             
 //            self.defaults.set(self.itemArray, forKey: "TodoListArray")
@@ -109,27 +99,23 @@ class TodoListViewController: UITableViewController {
     //MARK: Model manipulation methods
     
     func saveItems(){
-        let encoder = PropertyListEncoder()
+
         
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("error encoding item array. \(error)")
+            print("Error saving context. \(error)")
         }
         
         self.tableView.reloadData()
     }
     
     func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error decoding item array. \(error)")
-            }
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+        itemArray = try context.fetch(request)
+        } catch {
+            print("Error reading from context. \(error)")
         }
     }
     
